fe3a1f52e01949514544aa98f84a1f91d8d62a9e
Connie Park <conniedc1206@gmail.com>
2022-07-19 10:16:49 -0700

    commit tree 2198810d852f9a07c189b4ddb30cc6ceed1c736a

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ApplicationRecord
+    # All authors have a name/is expected to validate that :name cannot be empty/falsy
+    # is expected to validate that :name is case-sensitively unique
+    validates :name, presence: true, uniqueness: true
+    # is expected to validate that the length of :phone_number is 10
+    validates :phone_number, length: {is: 10}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    # is expected to validate that :title cannot be empty/falsy
+    validates :title, presence: true
+    # is expected to validate that the length of :content is at least 250
+    validates :content, length: { minimum: 250 }
+    # is expected to validate that the length of :summary is at most 250
+    validates :summary, length: { maximum: 250 }
+    # is expected to validate that :category is either ‹"Fiction"› or ‹"Non-Fiction"›
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    # Custom validations: title is clickbait -> is expected not to allow :title to be ‹"True Facts"›
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+        if title == "True Facts"
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
